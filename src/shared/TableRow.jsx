b2c49090155060fe04b4bdac475d324224e7e187
shabeeb k <shabeeb.k@ramzaula.com>
2025-04-24 23:07:14 +0300

    token

--- src/shared/TableRow.jsx
+++ src/shared/TableRow.jsx
@@ -4,7 +4,7 @@ const TableRow = ({ children, index, isLastRow }) => {
     <tr
       className={` border-b ${index % 2 === 1 ? "glass-card" : "dark-glass"} ${
         isLastRow ? "border-b-[3px]" : ""
-      } hover:bg-yellow-400 `}
+      } `}
     >
       {children}
     </tr>
--- src/utils/apiInterceptor.js
+++ src/utils/apiInterceptor.js
@@ -4,12 +4,14 @@ const baseURL = "https://polymer-nodejs.vercel.app/api";
 const axiosInstance = axios.create({
   baseURL: baseURL,
 });
+const a = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MDYyYTRkYTkxYjc4NTczNjhiZDk0YiIsImVtYWlsIjoiam9obi5kb2VAZXhhbXBsZS5jb20iLCJuYW1lIjoiSm9obiBEb2UiLCJyb2xlIjoic3VwZXJhZG1pbiIsImlhdCI6MTc0NTUwMjA3MiwiZXhwIjoxNzQ1Njc0ODcyLCJpc3MiOiJjb2RlLW94In0.s8CxupRiBuZeyQskxlBOLDEe1OqXA0-ksPuJqhSzol4"
 const token = localStorage.getItem("token") || null;
+
 
 axiosInstance.interceptors.request.use(
   function (config) {
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${a}`;
     }
     return config;
   },
